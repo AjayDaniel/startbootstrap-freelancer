d38211e0965d7983c7f73371847320e2e5874aba
AjayDaniel <bertyajay@gmail.com>
2017-07-28 18:23:04 -0500

    Contact Form, Gulp file
    Contact form integrated with aws sns
    Gulp file output to dist
    Contact form UI

--- gulpfile.js
+++ gulpfile.js
@@ -25,6 +25,7 @@ gulp.task('less', function() {
         .pipe(less())
         .pipe(header(banner, { pkg: pkg }))
         .pipe(gulp.dest('css'))
+        .pipe(gulp.dest('dist/css'))
         .pipe(browserSync.reload({
             stream: true
         }))
@@ -36,6 +37,7 @@ gulp.task('minify-css', ['less'], function() {
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('css'))
+        .pipe(gulp.dest('dist/css'))
         .pipe(browserSync.reload({
             stream: true
         }))
@@ -48,6 +50,7 @@ gulp.task('minify-js', function() {
         .pipe(header(banner, { pkg: pkg }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('js'))
+        .pipe(gulp.dest('dist/js'))
         .pipe(browserSync.reload({
             stream: true
         }))
@@ -57,9 +60,11 @@ gulp.task('minify-js', function() {
 gulp.task('copy', function() {
     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
         .pipe(gulp.dest('vendor/bootstrap'))
+        .pipe(gulp.dest('dist/vendor/bootstrap'))
 
-    gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+    gulp.src(['node_modules/jquery/dist/jquery.min.js'])
         .pipe(gulp.dest('vendor/jquery'))
+        .pipe(gulp.dest('dist/vendor/jquery'))
 
     gulp.src([
             'node_modules/font-awesome/**',
@@ -70,10 +75,21 @@ gulp.task('copy', function() {
             '!node_modules/font-awesome/*.json'
         ])
         .pipe(gulp.dest('vendor/font-awesome'))
-})
+        .pipe(gulp.dest('dist/vendor/font-awesome'))
+
+    gulp.src(['node_modules/aws-sdk/dist/aws-sdk.min.js'])
+        .pipe(gulp.dest('vendor/aws'))
+        .pipe(gulp.dest('dist/vendor/aws'))
+
+    gulp.src(['js/jqBootstrapValidation.js', 'js/contact_me.js'])
+      .pipe(gulp.dest('dist/js'))
+
+    gulp.src(['index.html'])
+        .pipe(gulp.dest('dist'))
+});
 
 // Run everything
-gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy']);
+gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy',]);
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
--- js/contact_me.js
+++ js/contact_me.js
@@ -9,7 +9,7 @@ $(function() {
             // Prevent spam click and default submit behaviour
             $("#btnSubmit").attr("disabled", true);
             event.preventDefault();
-            
+
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
@@ -20,41 +20,8 @@ $(function() {
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
-            $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
-                },
-                cache: false,
-                success: function() {
-                    // Enable button & show success message
-                    $("#btnSubmit").attr("disabled", false);
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
-
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            });
+            var emailBody = "You have received a new message from your website contact form.\n\nHere are the details:\n\nName:"+name+"\n\nEmail: "+email+"\n\nPhone: "+phone+"\n\nMessage:\n"+message;
+            contactSubmit.sendToTopic(emailBody);
         },
         filter: function() {
             return $(this).is(":visible");
@@ -65,6 +32,47 @@ $(function() {
         e.preventDefault();
         $(this).tab("show");
     });
+
+    var contactSubmit={};
+    // Initialize the Amazon Cognito credentials provider
+    AWS.config.region = 'us-east-1'; // Region
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: 'us-east-1:3bc6ad83-7160-4d5c-9067-f76d3c6438fa',
+    });
+    contactSubmit.sendToTopic = function(message) {
+          var sns = new AWS.SNS();
+          var params = {
+              //Message: 'Hello topic', /* required */
+              Message: message,
+              Subject: 'My Personal Website - contact form',
+              TopicArn: 'arn:aws:sns:us-east-1:134023072187:mywebsite_contact_form'
+          };
+          sns.publish(params, function(err, data) {
+              if (err){
+                // Fail message
+                $('#success').html("<div class='alert alert-danger'>");
+                $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    .append("</button>");
+                $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                $('#success > .alert-danger').append('</div>');
+                //clear all fields
+                $('#contactForm').trigger("reset");
+              }else{
+                // Enable button & show success message
+                $("#btnSubmit").attr("disabled", false);
+                $('#success').html("<div class='alert alert-success'>");
+                $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    .append("</button>");
+                $('#success > .alert-success')
+                    .append("<strong>Your message has been sent. </strong>");
+                $('#success > .alert-success')
+                    .append('</div>');
+
+                //clear all fields
+                $('#contactForm').trigger("reset");
+              }          // successful response
+          });
+    };
 });
 
 // When clicking on Full hide fail/success boxes
